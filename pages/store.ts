e848f6da00dfaa870bae8d4ede92ddef664be7ab
Abraham Ortega <AbrahamOrtegaM@outlook.com>
2024-12-10 14:02:57 -0600

    redux fix

--- pages/store.ts
+++ pages/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '@/features/cartSlice';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
     reducer: {
@@ -11,5 +10,9 @@ export const store = configureStore({
     }),
 });
 
-export const useAppDispatch: () => typeof store.dispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
+export type AppStore = typeof store
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = AppStore['dispatch']
+
